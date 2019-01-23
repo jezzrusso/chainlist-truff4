5557909c59c100176bba5cf9f84894fe72ccab10
xb203738 <p000jrusso@prservicos.com.br>
2019-01-22 22:43:33 -0200

    Adding frontend files to github pages

--- Training/chainlist/deployfrontend.cmd
+++ Training/chainlist/deployfrontend.cmd
@@ -1,5 +1,5 @@
-robocopy src ..\..\docs /e
-robocopy build\contracts ..\..\docs
+robocopy src \..\..\docs /e
+robocopy build\contracts \..\..\docs
 git add . 
 git commit -m "Adding frontend files to github pages"
 git push
